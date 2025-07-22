b0e66d7893bd127d59529119f845a7305fe4622d
Glorieux-Victor <victor.glorieuv@gmail.com>
2025-07-22 08:59:03 +0000

    good plots and params for matplotlib

--- fonctions.py
+++ fonctions.py
@@ -879,36 +879,3 @@ def the_fonction(best_SNR_dict) :
 #===============================================================================================================================================
 #===============================================================================================================================================
 #===============================================================================================================================================
-
-import matplotlib as mpl
-
-def plot_settings():
-    plt.style.use(['default'])
-    mpl.rcParams['xtick.labelsize'] = 12
-    mpl.rcParams['ytick.labelsize'] = 12
-    mpl.rcParams['axes.labelsize'] = 14
-    mpl.rcParams['axes.titlesize'] = 16
-    mpl.rcParams['legend.fontsize'] = 12
-    mpl.rcParams['font.size'] = 14  # global font size
-
-    mpl.rcParams['axes.grid'] = True
-    mpl.rcParams['axes.formatter.use_mathtext'] = True
-    mpl.rcParams['axes.formatter.limits'] = -2,3
-    mpl.rcParams['grid.linewidth'] = 0.4
-    mpl.rcParams['legend.fancybox'] = False
-    mpl.rcParams['legend.numpoints'] = 5
-    mpl.rcParams['legend.scatterpoints'] = 5
-    mpl.rcParams['legend.edgecolor'] = 'gray'
-    mpl.rcParams['patch.linewidth'] = 1
-
-    mpl.rcParams['legend.borderpad'] = 0.4
-    mpl.rcParams['legend.labelspacing'] = 0.5
-
-    mpl.rcParams['legend.handlelength'] = 1
-    mpl.rcParams['legend.handleheight'] = 0.6
-    mpl.rcParams['legend.handletextpad'] = 0.8
-    mpl.rcParams['legend.borderaxespad'] = 0.5
-    mpl.rcParams['legend.columnspacing'] = 2
-
-    mpl.rcParams['axes.edgecolor'] = 'gray'
-    mpl.rcParams['axes.linewidth'] = 0.8
